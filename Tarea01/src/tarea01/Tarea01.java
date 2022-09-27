2b2af7945a8ccc3ee6a2025891ac352040c16eeb
MattelXD <matiashernanxd@gmail.com>
2022-09-27 16:48:28 -0300

    Estructura del proyecto y primera clase main}

---
+++ Tarea01/src/tarea01/Tarea01.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tarea01;
+
+/**
+ *
+ * @author mabarriga2021
+ */
+public class Tarea01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
