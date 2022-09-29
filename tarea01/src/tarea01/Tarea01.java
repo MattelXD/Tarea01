40acab998f7b6721866c7fc0638a7b032fdb4912
ricachii <114707269+ricachii@users.noreply.github.com>
2022-09-29 22:37:20 -0100

    Update Tarea01.java

--- tarea01/src/tarea01/Tarea01.java
+++ tarea01/src/tarea01/Tarea01.java
@@ -1,20 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package tarea01;
 
-/**
- *
- * @author mabarriga2021
- */
-public class Tarea01 {
 
-    /**
-     * @param args the command line arguments
-     */
+
+
+
+public class Tarea01 {
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    }
 }
